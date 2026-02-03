0c074ce1098809d524ebd37ea9cfab629370df2f
Jaykwok2999 <61024435@qq.com>
2026-02-03 17:36:59 +0800

    Update diy-openwrt.sh

--- diy/diy-openwrt.sh
+++ diy/diy-openwrt.sh
@@ -148,7 +148,6 @@ git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-dockerma
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-upnp
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages miniupnpd
 git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
-#git clone https://github.com/Jaykwok2999/passwall-packages.git package/passwall_packages
 # git clone https://github.com/pymumu/luci-app-smartdns.git luci-app-smartdns
 # git clone https://github.com/pymumu/openwrt-smartdns.git smartdns
 
@@ -156,6 +155,8 @@ git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
 # SSRP & Passwall
 git_sparse_clone main https://github.com/kiddin9/kwrt-packages luci-app-passwall
 cp -af feeds/istoreos_ipk/patch/un.svg package/luci-app-passwall/root/www/luci-static/passwall/flags/
+
+git clone https://github.com/Jaykwok2999/passwall-packages.git package/passwall_packages
 
 # samba4
 #rm -rf feeds/packages/libs/liburing
